a2fa4471ba4f4f5f5a605c770ac165ee988ea8c5
Niewidzialny84 <36695711+Niewidzialny84@users.noreply.github.com>
2023-09-16 20:38:48 +0200

    Fixed domain

--- app/library/settings.py
+++ app/library/settings.py
@@ -5,4 +5,4 @@ config = dotenv_values("shared/.env")
 DISCORD_WEBHOOK = config["DISCORD_WEBHOOK"]
 LOGIN = config["LOGIN"].encode("utf-8")
 PASSWORD = config["PASSWORD"].encode("utf-8")
-DOMAIN = config["PASSWORD"]
+DOMAIN = config["DOMAIN"]
